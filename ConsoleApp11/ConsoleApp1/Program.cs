431f235c3593e9d1170c2e43104381db5a42d576
Alex30102003 <115226336+Alex30102003@users.noreply.github.com>
2022-10-07 16:44:19 +0500

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var array = new int[6000];
+
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+                array[i] = random.Next(int.MaxValue);
+
+            Console.WriteLine("Sorting...");
+            var stopWatch = Stopwatch.StartNew();
+            Sort(array);
+            stopWatch.Stop();
+
+            Check(array);
+            Console.WriteLine("Time: {0} ms", stopWatch.ElapsedMilliseconds);
+        }
+
+        private static void Sort(int[] array)
+        {
+            bool flag;
+            do
+            {
+                flag = false;
+                for (var i = 0; i < array.Length - 1; i++)
+                    if (array[i] > array[i + 1])
+                    {
+                        var temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        flag = true;
+                    }
+            }
+            while (flag);
+        }
+
+        private static void Check(int[] array)
+        {
+            var check = true;
+            for (var i = 0; i < array.Length - 1; i++)
+                if (array[i] > array[i + 1])
+                {
+                    check = false;
+                    break;
+                }
+            Console.WriteLine("Check array: {0}", check);
+        }
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace поиск
+{
+    public class Search
+    {
+        public static int SimpleSearching(int[] a, int x)
+        {
+            int i = 0;
+            while (i < a.Length && a[i] != x)
+                i++;
+            if (i < a.Length) return i;
+            else return -1;
+        }
+
+        public static int BinarySearching(int[] a, int x)
+        {
+            int middle, left = 0, right = a.Length - 1;
+            do
+            {
+                middle = (left + right) / 2;
+                if (x > a[middle])
+                    left = middle + 1;
+                else right = middle - 1;
+            }
+            while ((a[middle] != x) && (left <= right));
+            if (a[middle] == x) return middle;
+            else return -1;
+        }
+    }
+}
